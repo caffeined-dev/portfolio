2846db5d8f4aa3e9fb88b5acc0da5b206a411133
caffeinedttv <69026503+caffeinedttv@users.noreply.github.com>
2021-05-12 16:44:08 -0700

    Bootstrap

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './main.scss';
+import Header from './layout/header/header';
 
 export default function Main() {
   return (
     <div className="Main">
-      <div></div>
+      <Header />
       <div></div>
       <div></div>
       <img className="logo" src="./catlogo.png" alt="logo"/>
