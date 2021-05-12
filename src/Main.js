3ee57a8bbf9d9d78e0edd7a489b020883d963133
caffeinedttv <69026503+caffeinedttv@users.noreply.github.com>
2021-05-12 14:44:40 -0700

    First commit

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.scss';
+
+export default function Main() {
+  return (
+    <div className="Main">
+      <div></div>
+      <div></div>
+      <div></div>
+      <img className="logo" src="./catlogo.png" alt="logo"/>
+    </div>
+  );
+}
+
